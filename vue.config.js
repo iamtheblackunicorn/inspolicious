5df086747b18172193fdb9e520a4b27b0b874d09
iamtheblackunicorn <officialrealalexanderabraham@gmail.com>
2022-06-14 19:40:57 +0200

    Added a description.

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,4 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+// A small Vue.js app to inspire writers. :black_nib: :ribbon:
