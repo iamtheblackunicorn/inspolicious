1b75a602ce77a9726ae7c28d6b3c17d911875ee0
✭ THE BLACK UNICORN ✭ <78213234+iamtheblackunicorn@users.noreply.github.com>
2022-07-02 01:36:31 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
-// A small Vue.js app to inspire writers. :black_nib: :ribbon:
+module.exports = defineConfig(
+  {
+    transpileDependencies: true,
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/cmatch/'
+      : '/'
+  }
+);
